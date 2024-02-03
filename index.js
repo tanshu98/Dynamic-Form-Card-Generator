31e5e8d6d6201a50e86ea897bd37e78b7e977525
Tanshu Allewar <tanshuallewar98@gmail.com>
2024-02-03 12:55:22 +0530

    Removed Comments and console logs

--- index.js
+++ index.js
@@ -6,12 +6,8 @@ const input = document.querySelector("input");
 const cardContainer = document.querySelector(".card-container");
 const cardContainerID = document.querySelector("#card-container");
 let count = 0;
-const cardForm = document.querySelector('#card-form');
-// const modalContent = document.querySelector('.modal-content');
-// console.log(modalContent);
-const closeModal = document.querySelector('.close');
-console.log(closeModal);
-
+const cardForm = document.querySelector("#card-form");
+const closeModal = document.querySelector(".close");
 
 // Event listner on Generate Button
 generateCardButton.addEventListener("click", function (e) {
@@ -20,25 +16,22 @@ generateCardButton.addEventListener("click", function (e) {
   if (cardContainer.innerHTML) {
     cardContainer.innerHTML = "";
     generateCards();
-    console.log(card);
     input.value = "";
   } else {
     generateCards();
     input.value = "";
   }
-
-
 });
 
 // Event Listner on Reset Button -
 resetCardButton.addEventListener("click", (e) => {
   e.preventDefault();
   cardContainer.innerHTML = "";
+  input.value = "";
 });
 
 const generateCards = function () {
   let inputValue = input.value;
-  console.log(inputValue,'inputValue=========>');
   for (let i = 0; i < inputValue; i++) {
     let card = document.createElement("div");
     card.setAttribute("class", "card-class");
@@ -46,74 +39,63 @@ const generateCards = function () {
     card.setAttribute("backgroundColor", "red");
     // card.setAttribute("h4", "Heading-4");
     card.innerHTML = `Card ${i + 1}`;
-
-    let paraCard = document.createElement('p', '');
-    paraCard.style.margin = '10px';
+    
+    // Create Para element
+    let paraCard = document.createElement("p", "");
+    paraCard.style.margin = "10px";
     card.appendChild(paraCard);
-    card.style.cursor = 'pointer';
+    card.style.cursor = "pointer";
     cardContainerID.appendChild(card);
   }
   inputValue = "";
 };
 
-
-cardContainer.addEventListener('click', function(e) {
-  console.log(e.target.childNodes[1]);
-  console.log(e);
-
-  // Create ModalContent on click - 
-  let modalContent = document.createElement('div');
-  modalContent.setAttribute('class', 'modal-content');
+cardContainer.addEventListener("click", function (e) {
+  // Create ModalContent on click -
+  let modalContent = document.createElement("div");
+  modalContent.setAttribute("class", "modal-content");
   document.body.appendChild(modalContent);
 
-  let inputContent = document.createElement('input');
-  inputContent.setAttribute('type', 'text');
-  inputContent.setAttribute('placeholder', 'Jot down something');
+  let inputContent = document.createElement("input");
+  inputContent.setAttribute("type", "text");
+  inputContent.setAttribute("placeholder", "Jot down something");
   modalContent.appendChild(inputContent);
 
-  let spanContent = document.createElement('span');
-  spanContent.setAttribute('class', 'close');
-  spanContent.innerHTML = '&times;';
+  let spanContent = document.createElement("span");
+  spanContent.setAttribute("class", "close");
+  spanContent.innerHTML = "&times;";
   modalContent.appendChild(spanContent);
 
-  let submitButton = document.createElement('button');
-  submitButton.innerHTML = 'Submit';
-  submitButton.setAttribute('id', 'modal-btn');
+  let submitButton = document.createElement("button");
+  submitButton.innerHTML = "Submit";
+  submitButton.setAttribute("id", "modal-btn");
   modalContent.appendChild(submitButton);
 
   // Adding styling to created Modal Content Card
 
-  inputContent.style.outline = 'none';
-  inputContent.style.border = 'none';
-  inputContent.style.backgroundColor = 'transparent';
-  submitButton.style.padding = '10px';
+  inputContent.style.outline = "none";
+  inputContent.style.border = "none";
+  inputContent.style.backgroundColor = "transparent";
+  submitButton.style.padding = "10px";
 
   let dataObj = {
     id: e.target.id,
-    data: ""
+    data: "",
   };
 
-  // Logic for Storing data in the Modal Card after submit button
-
-  console.log(dataObj);
-
-
   // Store data in an object when user clicks on submit button
-  submitButton.addEventListener('click', function() {
-    console.log('clicked inside submit button');
+  submitButton.addEventListener("click", function () {
     dataObj.data = inputContent.value;
-    console.log('dataObj', dataObj);
 
     // Storing the modal submitted data in the resp card
 
     e.target.childNodes[1].innerHTML = dataObj.data;
-  modalContent.style.display = 'none';
+    modalContent.style.display = "none";
+  });
 
-  })
+  spanContent.addEventListener("click", function () {
+    modalContent.style.display = "none";
+  });
 
-  spanContent.addEventListener('click', function() {
-  modalContent.style.display = 'none';
-  })
-
-  modalContent.style.display = 'flex';
-})
+  modalContent.style.display = "flex";
+});
